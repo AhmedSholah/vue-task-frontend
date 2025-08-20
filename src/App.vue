e0a7aca4d594e090a279aab432eb9ea1dfad1784
Ahmed <midots202@gmail.com>
2025-08-21 02:04:38 +0300

    feat: add MovieCard and IconStar components to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import Header from "./components/Header/Header.vue";
+import IconStar from "./components/icons/IconStar.vue";
+import MovieCard from "./components/MovieCard.vue";
 </script>
 
 <template>
@@ -8,7 +10,15 @@ import Header from "./components/Header/Header.vue";
   </header>
 
   <main>
-    <div class="container"></div>
+    <div class="container">
+      <div class="grid sm:grid-cols-1 md:grid-cols-2 grid-cols-3 gap-4">
+        <MovieCard /><MovieCard /><MovieCard /><MovieCard />
+        <div class="relative">
+          <IconStar />
+          <span class="absolute">5</span>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
