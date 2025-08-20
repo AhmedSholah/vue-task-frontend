98115beed16213efb58b0f672511daa3418c66a8
Ahmed <midots202@gmail.com>
2025-08-20 23:33:14 +0300

    Refactor AddMovieModal component and integrate it into Header.vue

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,133 +1,62 @@
-<!-- <script lang="ts" setup>
-import { ref } from "vue";
-const open = ref<boolean>(false);
-
-const showModal = () => {
-  open.value = true;
-};
-
-const handleOk = (e: MouseEvent) => {
-  console.log(e);
-  open.value = false;
-};
-</script>
-
-<template>
-  <div class="container">
-    <a-flex
-      >Total Movies: 1 / Average Ratign: 3.7
-      <div>
-        <a-button type="primary" @click="showModal">Add Movie</a-button>
-      </div></a-flex
-    >
-  </div>
-  <a-modal v-model:open="open" title="Basic Modal" @ok="handleOk">
-    <p>Some contents...</p>
-    <p>Some contents...</p>
-    <p>Some contents...</p>
-  </a-modal>
-</template> -->
-
 <template>
   <div>
     <a-button type="primary" @click="showModal"> Add New Movie </a-button>
-
-    <a-modal
-      v-model:open="isModalVisible"
-      title="Add New Movie"
-      @cancel="handleCancel"
-      centered
-    >
-      <a-form :model="formState" layout="vertical" name="movie_form">
-        <a-form-item label="Name">
-          <a-input v-model:value="formState.name" />
-        </a-form-item>
-
-        <a-form-item label="Description">
-          <a-textarea v-model:value="formState.description" :rows="4" />
-        </a-form-item>
-
-        <a-form-item label="Image">
-          <a-input
-            v-model:value="formState.imageUrl"
-            placeholder="e.g., https://example.com/image.jpg"
-          />
-        </a-form-item>
-
-        <a-form-item label="Genres">
-          <a-select
-            v-model:value="formState.genres"
-            mode="multiple"
-            style="width: 100%"
-            placeholder="Select genres"
-          >
-            <a-select-option value="drama">Drama</a-select-option>
-            <a-select-option value="crime">Crime</a-select-option>
-            <a-select-option value="action">Action</a-select-option>
-            <a-select-option value="comedy">Comedy</a-select-option>
-            <a-select-option value="fantasy">Fantasy</a-select-option>
-            <a-select-option value="sci-fi">Sci-Fi</a-select-option>
-          </a-select>
-        </a-form-item>
-
-        <a-form-item>
-          <a-checkbox v-model:checked="formState.inTheaters">
-            In theaters
-          </a-checkbox>
-        </a-form-item>
-      </a-form>
-
-      <template #footer>
-        <a-button key="back" @click="handleCancel">Cancel</a-button>
-        <a-button
-          key="submit"
-          type="primary"
-          :loading="isLoading"
-          @click="handleOk"
-          >Create</a-button
-        >
-      </template>
-    </a-modal>
   </div>
+  <AddMovieModal
+    :opened="isModalVisible"
+    :form-state="formState"
+    :handle-cancel="handleCancel"
+    :onFinish="onFinish"
+    :onFinishFailed="onFinishFailed"
+  />
 </template>
 
-<script setup>
+<script setup lang="ts">
+import AddMovieModal from "./components/AddMovieModal.vue";
 import { ref, reactive } from "vue";
 
-// Controls the visibility of the modal
 const isModalVisible = ref(false);
 
-// Controls the loading state of the "Create" button
 const isLoading = ref(false);
 
-// Holds the data for all the form fields
-const formState = reactive({
-  name: "Lord of the Rings: The Return of the King",
+export interface FormState {
+  name: string;
+  description: string;
+  imageUrl: string;
+  genres: string[];
+  inTheaters: boolean;
+}
+
+const formState = reactive<FormState>({
+  name: "",
   description: "",
   imageUrl: "",
-  genres: [], // This will hold an array of selected genre values, e.g., ['action', 'fantasy']
+  genres: [],
   inTheaters: false,
 });
 
-// Function to open the modal
+const resetForm = () => {
+  formState.name = "";
+  formState.description = "";
+  formState.imageUrl = "";
+  formState.genres = [];
+  formState.inTheaters = false;
+};
+
 const showModal = () => {
   isModalVisible.value = true;
 };
 
-// Function to handle the "Create" button click
-const handleOk = () => {
-  console.log("Form State Submitted:", formState);
-  isLoading.value = true;
+const onFinish = (values: FormState) => {
+  console.log("Success:", values);
+};
 
-  // Simulate an API call
-  setTimeout(() => {
-    isLoading.value = false;
-    isModalVisible.value = false;
-    // Here you would typically reset the form or show a success message
-  }, 2000); // 2-second delay
+const onFinishFailed = (errorInfo: any) => {
+  console.log("Failed:", errorInfo);
 };
 
 const handleCancel = () => {
   isModalVisible.value = false;
+  resetForm();
 };
 </script>
---
+++ src/components/Header/components/AddMovieModal.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import type { FormState } from "../Header.vue";
+
+interface Props {
+  opened: boolean;
+  formState: FormState;
+  handleCancel: () => void;
+  onFinish: (values: FormState) => void;
+  onFinishFailed: (error: any) => void;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <a-modal
+    v-bind:open="opened"
+    title="Add New Movie"
+    @cancel="handleCancel"
+    centered
+    :footer="null"
+  >
+    <a-form
+      :model="formState"
+      layout="vertical"
+      name="movie_form"
+      autocomplete="off"
+      @finish="onFinish"
+      @finishFailed="onFinishFailed"
+    >
+      <a-form-item
+        label="Name"
+        name="name"
+        :rules="[{ required: true, message: 'Please provide a movie name' }]"
+      >
+        <a-input v-model:value="formState.name" placeholder="Enter name here" />
+      </a-form-item>
+
+      <a-form-item label="Description" name="description">
+        <a-textarea
+          v-model:value="formState.description"
+          :auto-size="{ minRows: 4, maxRows: 6 }"
+          placeholder="Enter description here"
+        />
+      </a-form-item>
+
+      <a-form-item
+        label="image"
+        name="imageUrl"
+        :rules="[{ required: true, message: 'Please provide an image URL' }]"
+      >
+        <a-input
+          v-model:value="formState.imageUrl"
+          placeholder="e.g., https://example.com/image.jpg"
+        />
+      </a-form-item>
+
+      <a-form-item label="Genres" name="genres">
+        <a-select
+          v-model:value="formState.genres"
+          mode="multiple"
+          style="width: 100%"
+          placeholder="Select genres"
+        >
+          <a-select-option value="drama">Drama</a-select-option>
+          <a-select-option value="crime">Crime</a-select-option>
+          <a-select-option value="action">Action</a-select-option>
+          <a-select-option value="comedy">Comedy</a-select-option>
+        </a-select>
+      </a-form-item>
+
+      <a-form-item name="inTheaters">
+        <a-checkbox v-model:checked="formState.inTheaters">
+          In theaters
+        </a-checkbox>
+      </a-form-item>
+
+      <a-form-item>
+        <a-button key="back" @click="handleCancel">Cancel</a-button>
+        <a-button
+          key="submit"
+          type="primary"
+          html-type="submit"
+          :loading="false"
+          >Create</a-button
+        >
+      </a-form-item>
+    </a-form>
+  </a-modal>
+</template>
