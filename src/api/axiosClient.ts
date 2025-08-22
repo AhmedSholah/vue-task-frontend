3779979df59200a81dfbd7c9fc178e4ce26611b0
Ahmed <midots202@gmail.com>
2025-08-22 04:53:47 +0300

    fix: update axiosClient baseURL for production environment

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://158.180.60.149:3000",
   headers: {
     "Content-Type": "application/json",
   },
