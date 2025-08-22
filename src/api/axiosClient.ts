355629ebaad0854298908228c928810a3221c455
Ahmed <midots202@gmail.com>
2025-08-22 05:52:54 +0300

    fix: correct axiosClient baseURL by removing trailing slash for consistency

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   // baseURL: "http://localhost:3000",
-  baseURL: "http://158.180.60.149:3000/",
+  baseURL: "http://158.180.60.149:3000",
   headers: {
     "Content-Type": "application/json",
   },
