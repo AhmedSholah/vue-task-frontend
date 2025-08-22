c3faa1bcf73191b84634feebc064563195270b6c
Ahmed <midots202@gmail.com>
2025-08-22 04:58:27 +0300

    feat: configure Vite server proxy and update axiosClient baseURL for API requests

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://158.180.60.149:3000",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
